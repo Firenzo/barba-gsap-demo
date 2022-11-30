d7022d7c24a4e9ec3fd267ac48246dfca131e287
Firenzo <firenzo_akkefietje@msn.com>
2022-11-30 03:07:44 +0100

    code optimizations

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ const blockSection = document.querySelector('section#blocks')
 const amountOfBlocks = 0
 const animationThing = document.querySelector('div.animation-thing')
 
-// if (blockSection && !!amountOfBlocks) {
-//   for (let i = 0; i < amountOfBlocks; i++) {
-//     blockSection.innerHTML += `
-//       <div class="block"></div>
-//     `
-//   }
-// }
+if (blockSection && !!amountOfBlocks) {
+  for (let i = 0; i < amountOfBlocks; i++) {
+    blockSection.innerHTML += `
+      <div class="block"></div>
+    `
+  }
+}
 
 // --------------------------------------------//
 //                                             //
@@ -27,105 +27,94 @@ const animationThing = document.querySelector('div.animation-thing')
 //                                             //
 // --------------------------------------------//
 
+const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches
 // 1. Basic intro animations
 
-// gsap.from(header, {
-//   opacity: 0,
-//   y: -header.offsetHeight,
-//   duration: 1,
-//   delay: 0.5,
-// })
+const tlHeader = gsap.timeline()
+const tlCards = gsap.timeline()
 
-// if (cardSection) {
-//   gsap.from('.card', {
-//     y: 200,
-//     opacity: 0,
-//     duration: 1,
-//     delay: 1,
-//     stagger: {
-//       each: 0.3,
-//     },
-//   })
-// }
+tlHeader.from(header, {
+  opacity: 0,
+  y: -header.offsetHeight,
+  duration: 1,
+  delay: 0.5,
+})
+tlHeader.to(header, {
+  background: 'rgba(44, 62, 80, 0)',
+  delay: 0.5,
+})
+
+if (cardSection) {
+  tlCards.from('.card', {
+    y: 200,
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+  })
+  tlCards.to('.card', {
+    scale: 1.2,
+  })
+}
 
 // 2. Row and Grid animations
-const showBlockAnimation = 0
-if (blockSection && amountOfBlocks && showBlockAnimation) {
+const playBlockAnimation = 0
+if (blockSection && amountOfBlocks && playBlockAnimation) {
   gsap.to('.block', {
-    y: 100,
     opacity: 0,
-    ease: 'power2.inOut',
-    duration: 7,
+    duration: 3,
+    delay: 0.5,
     stagger: {
-      each: 0.1,
+      each: 0.2,
       from: 'center',
       grid: 'auto',
-      duration: 0.5,
     },
   })
 }
 
-// barba.init({
-//   transitions: [
-//     {
-//       name: 'default-transition',
-//       leave(data) {
-//         return gsap.to('header', { y: -document.querySelector('header').offsetHeight, duration: 0.5 })
-//       },
-//       enter(data) {
-//         return gsap.to('header', { y: 0, delay: 0.7 })
-//       },
-//     },
-//   ],
-// })
-
-let tl = gsap.timeline()
-gsap.set('.animation-thing', {
-  y: '100%',
-})
+// 3. Page Transition
+const tlPageTransition = gsap.timeline()
 
 barba.init({
   transitions: [
     {
-      name: 'default-transition',
-      preventRunning: true,
-      enter(data) {
+      leave(data) {
         gsap.to(data.current.container, {
           opacity: 0,
-          duration: 0.2,
-          delay: 0.5,
+          duration: 0.5,
         })
-        // tl.set(animationThing, { top: '100%' })
-        // tl.to(animationThing, { top: 0, duration: 0.5 })
-        // tl.to(animationThing, { top: '-100%', duration: 0.5 })
-
-        gsap.set('#animation-things', { zIndex: 1000 })
-        tl.to('.animation-thing', {
+        gsap.set('#bars-wrapper', { zIndex: 1000 })
+        tlPageTransition.to('.bar', {
           y: 0,
           duration: 0.5,
           stagger: {
             each: 0.1,
           },
         })
-        tl.to('.animation-thing', {
+      },
+
+      enter(data) {
+        tlPageTransition.to('.bar', {
           y: '-100%',
           stagger: {
             each: 0.1,
           },
           onComplete: () => {
-            gsap.set('#animation-things', { zIndex: -1000 })
-            gsap.set('.animation-thing', { y: '100%' })
+            gsap.set('#bars-wrapper', { zIndex: -1000 })
+            gsap.set('.bar', { y: '100%' })
+            const cardSectionNextPage = data.next.container.querySelector('section#cards')
+            const cards = data.next.container.querySelectorAll('.card')
+
+            if (cardSectionNextPage) {
+              gsap.to(cards, {
+                scale: 1.2,
+              })
+            }
           },
         })
-
         return gsap.from(data.next.container, {
           opacity: 0,
-          duration: 0.1,
-          delay: 0.5,
-          onComplete: () => {
-            window.scrollTo({ top: 0, behavior: 'smooth' })
-            console.log('complete')
-          },
+          duration: 1,
+          delay: 1,
         })
       },
     },
